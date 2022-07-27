a71f1621881fcba2dffe31af1cb06c2ae919161d
Nii <bnstephens001@gmail.com>
2022-07-27 06:21:44 +0000

    Completed Assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  
+  const [inCart, setInCart] = useState(false)
+
+
+  function location(){
+    setInCart((inCart => !inCart))
+  }
+
+  const setClass = inCart ? "in-cart" : null
   return (
-    <li className="">
+    <li className={setClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={location}>{inCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
 
-export default Item;
+export default Item
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+ 
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  function saveNewCategory(e){
+    setSelectedCategory(e.target.value)
+  }
+  const categorySelection = items.filter((i) => {
+    if (selectedCategory === "All"){
+      return true
+    }else
+    {
+      return i.category === selectedCategory
+    }
+  })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={saveNewCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +28,11 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {categorySelection.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
   );
 }
-
-export default ShoppingList;
+export default ShoppingList
